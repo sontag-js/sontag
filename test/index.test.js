f0118a36d23a8d6882ae1b3123d1e6fae81775f8
Dan Burzo <dan@danburzo.ro>
2019-08-04 11:23:48 +0300

    Added template tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,29 @@
 import Enaml from '../src/index';
+import { sync } from 'fast-glob';
+import tape from 'tape';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-let env = new Enaml();
+let cwd = 'test/fixtures';
+let fixtures = sync('**/*.html', { cwd });
+let env = new Enaml(cwd);
 
-env.renderString(`
-	{% extends 'note.html' %}
-	{% block start %}
-		{% set x = 100 %}
-		Some {{ 1 + 2 }}
-		{% set y %}
-			Content of y
-		{% endset %}
-	{% endblock %}
-	{% block main %}
-		{# best content ever #}
-		<div>Other content</div>
-	{% endblock %}
-`).then(console.log);
+// strip trailing newlines
+const trim = str => str.replace(/(^\n*|\n$)/g, '');
+
+tape('fixtures', async t => {
+	t.plan(fixtures.length);
+	await Promise.all(fixtures.map(async template => {
+		let [ input, context, expected ] = readFileSync(join(cwd, template), 'utf8').split('---');
+		context = JSON.parse(context);
+		input = trim(input);
+		expected = trim(expected);
+		try {
+			let res = await env.renderString(input, context);
+			t.equal(trim(res), expected, template);
+		} catch(err) {
+			t.equal(err.toString(), expected, template);
+		}
+	}));
+	t.end();
+});
