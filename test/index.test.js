0976c64ca265b9228856b7071eb5f2b9206607a5
Dan Burzo <dan@danburzo.ro>
2025-09-11 22:59:19 +0300

    Fix tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,28 +1,32 @@
-import Sontag from '../src/index.js';
-import fg from 'fast-glob';
-import { readFileSync } from 'node:fs';
+import { readFileSync, globSync } from 'node:fs';
 import { join } from 'node:path';
 import assert from 'node:assert';
 import test from 'node:test';
 
+import Sontag from '../src/index.js';
+
 let cwd = 'test/fixtures';
-let fixtures = fg.sync('*/*.html', { cwd, ignore: ['*/_*.html'] });
+let fixtures = globSync('*/*.html', { cwd, exclude: ['*/_*.html'] });
 let env = new Sontag(cwd);
 
 // strip trailing newlines
 const trim = str => str.replace(/(^\n*|\n$)/g, '');
 
-test('fixtures', async t => {
-	await Promise.all(fixtures.map(async template => {
-		let [ input, context, expected ] = readFileSync(join(cwd, template), 'utf8').split('---');
+fixtures.forEach(template => {
+	test(template, async () => {
+		let [ input, context, expected ] = readFileSync(
+			join(cwd, template), 
+			'utf8'
+		).split('---');
 		context = new Function(`return ${context.trim()}`)();
 		input = trim(input);
 		expected = trim(expected);
+		let res;
 		try {
-			let res = await env.renderString(input, context);
-			assert.equal(trim(res), expected, template);
+			res = trim(await env.renderString(input, context));
 		} catch(err) {
-			assert.equal(err.toString(), expected, template);
+			res = err.toString();
 		}
-	}));
+		assert.equal(trim(res), expected);	
+	});
 });
